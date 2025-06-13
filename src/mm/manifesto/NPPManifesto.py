df7f30eea95bc67446db32dec97c6bfcea289127
nuuuwan <nuuuwan@gmail.com>
2025-06-13 14:14:13 +0530

    -> l2_topic

--- src/mm/manifesto/NPPManifesto.py
+++ src/mm/manifesto/NPPManifesto.py
@@ -1,9 +1,13 @@
+from mm.manifesto.NPPManifestoBase import NPPManifestoBase
 from mm.manifesto.NPPManifestoParser import NPPManifestoParser
 from mm.manifesto.NPPManifestoParserContents import NPPManifestoParserContents
 from mm.manifesto.NPPManifestoParserDetails import NPPManifestoParserDetails
 
 
 class NPPManifesto(
-    NPPManifestoParser, NPPManifestoParserContents, NPPManifestoParserDetails
+    NPPManifestoBase,
+    NPPManifestoParser,
+    NPPManifestoParserContents,
+    NPPManifestoParserDetails,
 ):
     pass
