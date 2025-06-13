b2e5238f751abb7f72e6d608a50726b2d3a62578
nuuuwan <nuuuwan@gmail.com>
2025-06-13 10:31:05 +0530

    Basic NPPManifesto parser

---
+++ src/mm/__init__.py
@@ -0,0 +1,3 @@
+# mm (auto generate by build_inits.py)
+
+from mm.manifesto import NPPManifesto
---
+++ src/mm/manifesto/__init__.py
@@ -0,0 +1,3 @@
+# mm.manifesto (auto generate by build_inits.py)
+
+from mm.manifesto.NPPManifesto import NPPManifesto
