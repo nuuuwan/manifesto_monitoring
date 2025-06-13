6c3c270ab0ef3115c18614d7f53222443e4ee4e5
nuuuwan <nuuuwan@gmail.com>
2025-06-13 14:30:14 +0530

    test_manifesto_pdf.py passes

---
+++ tests/test_manifesto_pdf.py
@@ -0,0 +1,13 @@
+import unittest
+from mm import NPPManifestoPDF
+
+TEST_MANIFESTO_PDF = NPPManifestoPDF()
+
+
+class TestCase(unittest.TestCase):
+    def test_raw_text(self):
+        self.assertEqual(len(TEST_MANIFESTO_PDF.raw_text), 275_868)
+
+    def test_lines(self):
+        lines = TEST_MANIFESTO_PDF.lines
+        self.assertEqual(len(lines), 4_377)
