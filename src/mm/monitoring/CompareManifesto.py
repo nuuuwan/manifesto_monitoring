c4fed8f852a95311d07273f9f40e3548859f654d
nuuuwan <nuuuwan@gmail.com>
2025-06-17 16:23:09 +0530

    removed print

--- src/mm/monitoring/CompareManifesto.py
+++ src/mm/monitoring/CompareManifesto.py
@@ -30,9 +30,7 @@ class CompareManifesto:
         if self.MAX_CABINET_DECISIONS and self.MAX_CABINET_DECISIONS < len(
             cabinet_decisions
         ):
-            cabinet_decisions = cabinet_decisions[
-                : self.MAX_CABINET_DECISIONS
-            ]
+            cabinet_decisions = cabinet_decisions[: self.MAX_CABINET_DECISIONS]
 
         return cabinet_decisions
 
@@ -122,7 +120,7 @@ class CompareManifesto:
             data_list.append(data)
         log.info(
             f"Found {
-                len(data_list)} high similarity pairs with min_sim={min_sim}"
+                len(data_list)} similarity data items with sim >= {min_sim}"
         )
-        print(data_list[0])
+
         return data_list
