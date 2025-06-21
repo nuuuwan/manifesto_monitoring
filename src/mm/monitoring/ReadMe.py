939951cbf873dec04807a318db2457fbc98aecb5
nuuuwan <nuuuwan@gmail.com>
2025-06-21 11:03:29 +0530

    AI Comparison of  1,345 NPP Manifesto Promises & 421 NPP Cabinet Decisions

--- src/mm/monitoring/ReadMe.py
+++ src/mm/monitoring/ReadMe.py
@@ -15,6 +15,14 @@ class ReadMe:
     README_PATH = "README.md"
     SOURCE_URL = "https://www.npp.lk/up/policies/en/npppolicystatement.pdf"
 
+    def __init__(self):
+        self.data_list = CompareManifesto().high_similarity_pairs
+        self.manifesto_idx = NPPManifestoPDF().get_manifesto().all_idx
+        self.manifesto_to_datalist = {
+            x["manifesto_key"]: x for x in self.data_list
+        }
+        self.cabinet_decision_idx = CabinetDecision.idx()
+
     @cached_property
     def header_lines(self):
         return [
@@ -42,82 +50,88 @@ class ReadMe:
         ]
 
     @staticmethod
+    def get_header_cells(col: str) -> str:
+        if col in ["Manifesto", "Cabinet Decision", "l1_topic", "Group"]:
+            return ":--"
+        return "--:"
+
+    @staticmethod
     def build_markdown_table(data_list: list[dict]) -> str:
         header = list(data_list[0].keys())
         header_row = " | ".join(header)
-        separator_row = " | ".join(["---"] * len(header))
+        separator_row = " | ".join(
+            [ReadMe.get_header_cells(col) for col in header]
+        )
         data_rows = "\n".join(
             " | ".join(str(row[col]) for col in header) for row in data_list
         )
 
         return f"{header_row}\n{separator_row}\n{data_rows}"
 
-    # flake8: noqa: C901 - HACK!
-    @cached_property
-    def compare_summary_lines(self):
+    @staticmethod
+    def get_sim_data_summary(old_d):
+        group_to_n = {}
+        for sim in old_d["sim_list"]:
+            group = ReadMe.get_group(sim)
+            group_to_n[group] = group_to_n.get(group, 0) + 1
 
-        data_list = CompareManifesto().high_similarity_pairs
-        manifesto_idx = NPPManifestoPDF().get_manifesto().all_idx
-        manifesto_to_datalist = {x["manifesto_key"]: x for x in data_list}
+        n = old_d["n"]
+        d = {
+            "l1": old_d["l1"],
+            "l1_topic": old_d["l1_topic"],
+            "n": n,
+        }
+        for group in ReadMe.THRESHOLDS:
+            n_group = group_to_n.get(group, 0)
+            p_group = n_group / n
+            formatted_data = (
+                f"{n_group} ({p_group:.0%})" if n_group > 0 else "-"
+            )
+            d[ReadMe.get_group_title(group)] = formatted_data
+        return d
+
+    @cached_property
+    def summary_data(self):  # noqa: C901
 
         idx = {
             "all": {
                 "l1": "all",
                 "l1_topic": "all",
                 "n": 0,
-                "⚪ 0.5 - 0.6": 0,
-                "🟡 0.6 - 0.7": 0,
-                "🟢 0.7 < ": 0,
+                "sim_list": [],
             }
         }
-        for manifesto_key, manifesto in manifesto_idx.items():
+        for manifesto_key, manifesto in self.manifesto_idx.items():
             l1 = manifesto_key[:1]
             if l1 not in idx:
                 idx[l1] = {
                     "l1": l1,
                     "l1_topic": manifesto["l1_topic"],
                     "n": 0,
-                    "⚪ 0.5 - 0.6": 0,
-                    "🟡 0.6 - 0.7": 0,
-                    "🟢 0.7 < ": 0,
+                    "sim_list": [],
                 }
 
             for k in ["all", l1]:
                 idx[k]["n"] += 1
-                sim_data = manifesto_to_datalist.get(manifesto_key)
-                if sim_data:
-                    similarity = sim_data["similarity"]
-                    if similarity > 0.7:
-                        idx[k]["🟢 0.7 < "] += 1
-                    elif similarity > 0.6:
-                        idx[k]["🟡 0.6 - 0.7"] += 1
-                    else:
-                        idx[k]["⚪ 0.5 - 0.6"] += 1
-
-        d_list = [x[1] for x in sorted(list(idx.items()), key=lambda x: x[0])]
+                sim_data = self.manifesto_to_datalist.get(manifesto_key)
+                if not sim_data:
+                    continue
+                similarity = sim_data["similarity"]
+                idx[k]["sim_list"].append(similarity)
 
         new_d_list = []
-        for d in d_list:
+        for d in idx.values():
+            new_d_list.append(ReadMe.get_sim_data_summary(d))
 
-            def f(x):
-                p = d[x] / d["n"]
-                return f"{p:.0%}"
+        new_d_list.sort(key=lambda x: (x["l1"]))
+        return new_d_list
 
-            new_d_list.append(
-                {
-                    "l1": d["l1"],
-                    "l1_topic": d["l1_topic"],
-                    "n": d["n"],
-                    "⚪ 0.5 - 0.6": f("⚪ 0.5 - 0.6"),
-                    "🟡 0.6 - 0.7": f("🟡 0.6 - 0.7"),
-                    "🟢 0.7 < ": f("🟢 0.7 < "),
-                }
-            )
-
+    @cached_property
+    def compare_summary_lines(self):
         return [
             "### Summary",
             "",
-            self.build_markdown_table(new_d_list),
+            self.build_markdown_table(self.summary_data),
             "",
         ]
 
@@ -140,14 +154,47 @@ class ReadMe:
             ]
         )
 
+    THRESHOLDS = {
+        "high": 0.7,
+        "medium": 0.6,
+        "low": 0.5,
+    }
+
+    EMOJIS = {
+        "high": "🟢",
+        "medium": "🟡",
+        "low": "🟠",
+        "nil": "🔴",
+    }
+
+    @staticmethod
+    def get_group_title(group):
+        return f"{ReadMe.EMOJIS[group]} {group.capitalize()}"
+
+    @staticmethod
+    def get_group(similarity):
+        for group, threshold in ReadMe.THRESHOLDS.items():
+            if similarity >= threshold:
+                return group
+        raise ValueError(f"Invalid similarity value: {similarity}. ")
+
+    @staticmethod
+    def get_similarity_threshold_legend_lines():
+        d_list = []
+        for group, threshold in ReadMe.THRESHOLDS.items():
+            d_list.append(
+                {
+                    "Group": ReadMe.get_group_title(group),
+                    "Threshold": f"{threshold:.0%}",
+                }
+            )
+        return [ReadMe.build_markdown_table(d_list), ""]
+
     @staticmethod
     def get_similarity_markdown(similarity):
-        emoji = "⚪"
-        if similarity >= 0.7:
-            emoji = "🟢"
-        elif similarity >= 0.6:
-            emoji = "🟡"
-        return f"{emoji} {similarity:.2f}"
+        group = ReadMe.get_group(similarity)
+        emoji = ReadMe.EMOJIS[group]
+        return f"{emoji} {similarity:.0%}"
 
     @staticmethod
     def get_table_data(x, manifesto_idx, cabinet_decision_idx):
@@ -164,13 +211,11 @@ class ReadMe:
 
     @cached_property
     def compare_data_lines(self):
-        data_list = CompareManifesto().high_similarity_pairs
-        manifesto_idx = NPPManifestoPDF().get_manifesto().all_idx
-        cabinet_decision_idx = CabinetDecision.idx()
-
         table_data_list = [
-            self.get_table_data(x, manifesto_idx, cabinet_decision_idx)
-            for x in data_list
+            self.get_table_data(
+                x, self.manifesto_idx, self.cabinet_decision_idx
+            )
+            for x in self.data_list
         ]
 
         table_data_list.sort(key=lambda x: (x["Similarity"],), reverse=True)
@@ -211,6 +256,7 @@ class ReadMe:
                     EmbeddingStore.MODEL_URL}) Model.",
                 "",
             ]
+            + self.get_similarity_threshold_legend_lines()
             + self.compare_summary_lines
             + self.compare_detail_lines
         )
