1cc6abf8d6e64b26a0a156024c6b9348ef07bffb
nuuuwan <nuuuwan@gmail.com>
2025-06-22 09:39:21 +0530

    test_manifesto_items, test_cabinet_decisions

--- tests/test_compare_manifesto.py
+++ tests/test_compare_manifesto.py
@@ -2,11 +2,20 @@ import unittest
 
 from mm import CompareManifesto
 
+TEST_COMPARE_MANIFESTO = CompareManifesto()
+
 
 class TestCase(unittest.TestCase):
 
+    def test_manifesto_items(self):
+        self.assertEqual(TEST_COMPARE_MANIFESTO.n_manifesto_items, 1345)
+
+    @unittest.skip("n_cabinet_decisions changes as more decisiosn are made.")
+    def test_cabinet_decisions(self):
+        self.assertEqual(TEST_COMPARE_MANIFESTO.n_cabinet_decisions, 421)
+
     def test_similarity_matrix(self):
-        self.assertIsNotNone(CompareManifesto().similarity_matrix)
+        self.assertIsNotNone(TEST_COMPARE_MANIFESTO.similarity_matrix)
 
     def test_high_similarity_pairs(self):
-        self.assertIsNotNone(CompareManifesto().high_similarity_pairs)
+        self.assertIsNotNone(TEST_COMPARE_MANIFESTO.high_similarity_pairs)
